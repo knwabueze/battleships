712d409e2231c6e451a57a26fcc322b77bdd1248
Kosi Nwabueze <kosinwabueze@gmail.com>
2017-06-27 15:24:56 -0700

    Initialized Unit Tests; Initialized Web API Project; Issue #6

---
+++ api/api.Tests/HTTP/UsersControllerHTTPTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Tests.HTTP
+{
+    [TestClass]
+    public class UsersControllerHTTPTests
+    {
+        [TestMethod]
+        public async Task RegisterPasssingTest()
+        {
+            using (var client = new HttpClient())
+            {
+
+            }
+        }
+    }
+}
